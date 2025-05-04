570303225dcecc75bd47251e62b62468dacf562e
gaellebega <gaellebega@gmail.com>
2025-05-04 08:02:20 +0200

    last change

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: './', // Make sure Vite uses the root folder
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure the output directory is 'dist'
-  }
+  plugins: [react()]
 })
